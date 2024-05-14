157fc82e41977f05c988e6e09135ae719d81c97c
Pavel Schoffer <xschof00@gmail.com>
2024-05-14 09:11:50 +0200

    Update firebaseHookst.ts

--- src/hooks/firebaseHookst.ts
+++ src/hooks/firebaseHookst.ts
@@ -3,32 +3,44 @@ import { useEffect, useState } from "react"
 import { useFirestore } from "reactfire";
 import { fromFirebaseDoc, fromFirebaseDocs } from "../utils/shared/firebase";
 
-export const useFirebaseDoc = <T>(path: string): [T | null, Function] => {
-    const [document, setDocument] = useState<T | null>(null)
+export const useFirebaseDoc = <T>(path: string): [T | null, Function, boolean] => {
+    const [state, setState] = useState<{ document: T | null, loaded: boolean }>({ document: null, loaded: false })
     const firestore = useFirestore();
 
     useEffect(() => {
+        const pathParts = path.split('/');
+        if (pathParts.length % 2 !== 0 || !pathParts[pathParts.length - 1]) {
+            return;
+        }
+
         return onSnapshot(doc(firestore, path), snap => {
             const result = fromFirebaseDoc<T>(snap);
-            setDocument(result)
+            setState({ document: result, loaded: true })
         })
     }, [path, firestore])
 
-    return [document, setDocument]
+    return [
+        state.document,
+        (newDocument: T) => setState({ document: newDocument, loaded: true }),
+        state.loaded
+    ]
 }
 
-export const useFirebaseQuery = <T>(path: string, ...queryConstraints: QueryConstraint[]): [T[], Function] => {
-    const [documents, setDocuments] = useState<T[]>([])
+export const useFirebaseQuery = <T>(path: string, ...queryConstraints: QueryConstraint[]): [T[], Function, boolean] => {
+    const [state, setState] = useState<{ documents: T[], loaded: boolean }>({ documents: [], loaded: false })
     const firestore = useFirestore();
 
     useEffect(() => {
         const q = query(collection(firestore, path), ...queryConstraints)
         return onSnapshot(q, snap => {
             const result = fromFirebaseDocs<T>(snap.docs);
-            setDocuments(result)
+            setState({ documents: result, loaded: true })
         });
         // eslint-disable-next-line
     }, [path, firestore]);
 
-    return [documents, setDocuments]
+    return [
+        state.documents,
+        (newDocuments: T[]) => setState({ documents: newDocuments, loaded: true }),
+        state.loaded]
 }
