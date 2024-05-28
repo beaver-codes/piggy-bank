1dfb321248daa7f0b35b52673c24fcabae76185c
Pavel Schoffer <schoffer.pavel@gmail.com>
2024-05-28 14:40:14 +0200

    add functions auth

--- functions/src/utils/api.ts
+++ functions/src/utils/api.ts
@@ -1,4 +1,6 @@
 import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+import { getConfig } from "./firebase";
 
 export const apiWrapper = (request: functions.https.Request,
     response: functions.Response<any>,
@@ -22,4 +24,36 @@ export const apiWrapper = (request: functions.https.Request,
             resolve();
         });
     })
+}
+
+
+interface RequireAuthResult {
+    uid: string,
+    email: string,
+}
+export const requireAuth = async (request: functions.https.Request): Promise<RequireAuthResult> => {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) {
+        throw new Error('auth: Missing auth header');
+    }
+    if (authHeader.startsWith('Bearer ')) {
+        const auth = admin.auth();
+        const token = authHeader.split('Bearer ')[1];
+        try {
+            const result = await auth.verifyIdToken(token);
+            return { uid: result.uid, email: result.email + '' };
+        } catch (error) {
+            throw new Error('auth: Invalid token');
+        }
+    } else if (authHeader.startsWith('Master ')) {
+        const config = await getConfig();
+        const masterToken = authHeader.split('Master ')[1];
+        if (masterToken && masterToken === config.masterKey) {
+            return { uid: 'master', email: 'master' };
+        }
+        throw new Error('auth: Invalid master token');
+    }
+
+
+    throw new Error('auth: Invalid auth header');
 }
